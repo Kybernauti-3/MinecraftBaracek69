03b0105ccba4557e172a2ca8a2a473297b2c81ab
mates-exe <matej9jun@seznam.cz>
2022-11-02 13:11:25 +0100

    Update servo.py

--- rpi ovladani domu/servo.py
+++ rpi ovladani domu/servo.py
@@ -5,9 +5,21 @@ servo = PWM(Pin(0))
 
 servo.freq(50)
 
-while True:
-    servo.duty_u16(1350)
-    utime.sleep(2)
+def dvere_otevrit():
+    servo.duty_u16(1475)
 
+def dvere_zavrit():
     servo.duty_u16(4100)
-    utime.sleep(2)
+
+while True:
+    control = input('''
+ovladani dveri: 
+[1] otevrit
+[2] zavrit
+
+''')
+
+    if(str(control) == "1"):
+        dvere_otevrit()
+    if(str(control) == "2"):
+        dvere_zavrit()

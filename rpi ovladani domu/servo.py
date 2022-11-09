1416b591c4d8f7e4780c5c162783c27674edda61
mates-exe <matej9jun@seznam.cz>
2022-11-09 12:43:54 +0100

    servo na tlacitko
    update

--- rpi ovladani domu/servo.py
+++ rpi ovladani domu/servo.py
@@ -2,24 +2,27 @@ from machine import Pin, PWM
 import utime
 
 servo = PWM(Pin(0))
-
 servo.freq(50)
 
+stav = 0
+
+button = Pin(1, Pin.IN, Pin.PULL_DOWN)
+
 def dvere_otevrit():
+    
     servo.duty_u16(1475)
 
 def dvere_zavrit():
     servo.duty_u16(4100)
 
-while True:
-    control = input('''
-ovladani dveri: 
-[1] otevrit
-[2] zavrit
-
-''')
-
-    if(str(control) == "1"):
-        dvere_otevrit()
-    if(str(control) == "2"):
+def door_switch(stav):
+    if(stav == 1):
         dvere_zavrit()
+    else:
+        dvere_otevrit()
+    return not stav
+
+
+while True:
+    if button.value():
+        stav = door_switch(stav)

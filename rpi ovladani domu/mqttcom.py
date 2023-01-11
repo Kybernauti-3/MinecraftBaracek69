3ca37bd6f1e0c5d3e53a21dbca8739a28fbd58fe
mates-exe <67760956+mates-exe@users.noreply.github.com>
2023-01-11 14:05:23 +0100

    xx
    xx

--- rpi ovladani domu/mqttcom.py
+++ rpi ovladani domu/mqttcom.py
@@ -1,4 +1,38 @@
-from servoledka import dvere_otevrit,dvere_zavrit
+#from housectrl import dvere_otevrit,dvere_zavrit
+
+import network
+from umqtt.simple import MQTTClient
+
+# 147.228.121.4:80
+
+broker = 'broker.hivemq.com'
+topic = "minecraftbaracek"
+
+def on_message(topic, msg):
+    print("Received message on topic: {}, with payload: {}".format(topic, msg))
 
 
-# https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
+def main():
+    # Connect to Wi-Fi
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect("kokot", "minecraft69")
+    print("pripojuji wajfaj")
+    while not wlan.isconnected():
+        pass
+    print("pripojeno wajfaj")
+    # Connect to MQTT broker
+    client = MQTTClient("Pico", broker)
+    client.connect()
+    print("pripojeno mqtt")
+
+    # Publish a message to an MQTT topic
+    client.publish(topic, b"Hello, MQTT!")
+    #client.disconnect()
+
+    while True:
+        # Check for incoming messages
+        #client.check_msg()
+        pass
+
+main()

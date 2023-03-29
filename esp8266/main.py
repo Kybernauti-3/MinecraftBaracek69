f7b7347887527c358f540d6b655073644da41cf5
mates-exe <67760956+mates-exe@users.noreply.github.com>
2023-03-29 14:08:42 +0200

    jj
    jjj

--- esp8266/main.py
+++ esp8266/main.py
@@ -1,9 +1,36 @@
 # main.py -- put your code here!
 
+
+print('jsem v mainu')
+import time
+print('spim 5s')
+time.sleep(5)
+print('vyspano')
+
+
+from machine import Pin
+blue = Pin(5, Pin.OUT)
+green = Pin(4, Pin.OUT)
+red = Pin(2, Pin.OUT)
+
+def rozni():
+  blue.value(1)
+  green.value(1)
+  red.value(1)
+
+def zhasni():
+  blue.value(0)
+  green.value(0)
+  red.value(0)
+
 def sub_cb(topic, msg):
   print((topic, msg))
   if topic == b'minecraftbaracek' and msg == b'svetloon':
     print('Svetlo zapnute')
+    rozni()
+  if topic == b'minecraftbaracek' and msg == b'svetlooff':
+    print('Svetlo vypnute')
+    zhasni()
 
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub, topic_pub
